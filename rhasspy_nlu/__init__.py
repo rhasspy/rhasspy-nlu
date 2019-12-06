ef1082bc8bb9245c38a8b47d308b9ed5d54c8f21
Michael Hansen <michael.hansen.24@us.af.mil>
2019-12-06 16:00:56 -0500

    Add language model mixing

--- rhasspy_nlu/__init__.py
+++ rhasspy_nlu/__init__.py
@@ -1,4 +1,6 @@
 """Utilities for Rhasspy natural language understanding."""
 
 from .ini_jsgf import parse_ini
-from .jsgf_graph import intents_to_graph
+from .jsgf_graph import intents_to_graph, graph_to_fst
+from .fsticuffs import recognize
+from .arpa_lm import fst_to_arpa, arpa_to_fst
--- rhasspy_nlu/arpa_lm.py
+++ rhasspy_nlu/arpa_lm.py
@@ -15,15 +15,34 @@ def fst_to_arpa(
     arpa_path: Path,
     **kwargs,
 ):
+    """Convert text FST to ARPA language model using opengrm."""
     for task in fst_to_arpa_tasks(
         fst_text_path, isymbols_path, osymbols_path, arpa_path, **kwargs
     ):
-        for action in task["actions"]:
-            file_dep = " ".join(f'"{d}"' for d in task.get("file_dep", []))
-            targets = " ".join(f'"{t}"' for t in task.get("targets", []))
-            command = action % {"dependencies": file_dep, "targets": targets}
-            _LOGGER.debug(command)
-            subprocess.check_call(command, shell=True)
+        run_task(task)
+
+
+# -----------------------------------------------------------------------------
+
+
+def arpa_to_fst(arpa_path: typing.Union[str, Path], fst_path: typing.Union[str, Path]):
+    """Convert ARPA language model to FST. Typically for language model mixing."""
+    run_task(arpa_to_fst_task(arpa_path, fst_path))
+
+
+def arpa_to_fst_task(
+    arpa_path: typing.Union[str, Path], fst_path: typing.Union[str, Path]
+) -> typing.Dict[str, typing.Any]:
+    """Generate doit compatible task for ARPA to FST conversion."""
+    return {
+        "name": "base_lm_to_fst",
+        "file_dep": [arpa_path],
+        "targets": [fst_path],
+        "actions": ["ngramread --ARPA %(dependencies)s %(targets)s"],
+    }
+
+
+# -----------------------------------------------------------------------------
 
 
 def fst_to_arpa_tasks(
@@ -34,7 +53,12 @@ def fst_to_arpa_tasks(
     fst_path: typing.Optional[typing.Union[str, Path]] = None,
     counts_path: typing.Optional[typing.Union[str, Path]] = None,
     model_path: typing.Optional[typing.Union[str, Path]] = None,
+    base_fst_weight: typing.Optional[
+        typing.Tuple[typing.Union[str, Path], float]
+    ] = None,
+    merge_path: typing.Optional[typing.Union[str, Path]] = None,
 ) -> typing.Iterable[typing.Dict[str, typing.Any]]:
+    """Generate doit compatible tasks for FST to ARPA conversion."""
     # Text -> FST
     fst_text_path = Path(fst_text_path)
     fst_path = Path(fst_path or (fst_text_path.parent / (fst_text_path.stem + ".fst")))
@@ -69,6 +93,27 @@ def fst_to_arpa_tasks(
         "actions": ["ngrammake %(dependencies)s %(targets)s"],
     }
 
+    if base_fst_weight:
+        # Mixed language modeling
+        base_path, base_weight = base_fst_weight
+        if base_weight > 0:
+            merge_path = merge_path or Path(str(fst_path) + ".merge")
+
+            # merge
+            yield {
+                "name": "lm_merge",
+                "file_dep": [base_path, model_path],
+                "targets": [merge_path],
+                "actions": [
+                    "ngrammerge "
+                    f"--alpha={base_weight} "
+                    "%(dependencies)s %(targets)s"
+                ],
+            }
+
+            # Use merged model instead
+            model_path = merge_path
+
     # model -> ARPA
     yield {
         "name": "intent_arpa",
@@ -76,3 +121,16 @@ def fst_to_arpa_tasks(
         "targets": [arpa_path],
         "actions": ["ngramprint --ARPA %(dependencies)s > %(targets)s"],
     }
+
+
+# -----------------------------------------------------------------------------
+
+
+def run_task(task: typing.Dict[str, typing.Any]):
+    """Execute a doit compatible task."""
+    for action in task["actions"]:
+        file_dep = " ".join(f'"{d}"' for d in task.get("file_dep", []))
+        targets = " ".join(f'"{t}"' for t in task.get("targets", []))
+        command = action % {"dependencies": file_dep, "targets": targets}
+        _LOGGER.debug(command)
+        subprocess.check_call(command, shell=True)
