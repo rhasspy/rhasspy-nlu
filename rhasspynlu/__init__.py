dc225e2993569a6a26dff23363b6e36ca267cb11
Michael Hansen <michael.hansen.24@us.af.mil>
2020-03-24 14:06:01 -0400

    Expose utility methods just added

--- rhasspynlu/__init__.py
+++ rhasspynlu/__init__.py
@@ -7,7 +7,9 @@ from .ini_jsgf import parse_ini
 from .jsgf import Rule, Sentence
 from .jsgf_graph import (
     graph_to_fst,
+    graph_to_gzip_pickle,
     graph_to_json,
+    gzip_pickle_to_graph,
     intents_to_graph,
     json_to_graph,
     sentences_to_graph,
